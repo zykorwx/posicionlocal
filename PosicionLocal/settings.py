24f71b0474b73f2606c91510d342774c2381129a
Zykor <enrique.wx@gmail.com>
2013-02-21 22:25:43 -0600

    datos de la base de datos

--- PosicionLocal/settings.py
+++ PosicionLocal/settings.py
@@ -17,10 +17,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'DB_PL',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'NAME': 'db_pl',                      # Or path to database file if using sqlite3.
+        'USER': 'viewor',                      # Not used with sqlite3.
+        'PASSWORD': 'viewor_12',                  # Not used with sqlite3.
+        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
